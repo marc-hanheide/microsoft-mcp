f710609943567492914eafd016f8919d742c4b39
Marc Hanheide <marc@hanheide.net>
2025-09-04 13:39:59 +0100

    fix: update token cache path to use user home directory for AzureAuthentication

--- src/microsoft_mcp/auth.py
+++ src/microsoft_mcp/auth.py
@@ -165,7 +165,7 @@ class AzureAuthentication:
 
         # Configure persistent token cache
         token_cache = TokenCachePersistenceOptions(
-            allow_unencrypted_storage=True, name="/Users/mhanheide/.azure-token-cache"
+            allow_unencrypted_storage=True, name=Path.home() / ".azure-token-cache"
         )
 
         # Try to load existing AuthenticationRecord
